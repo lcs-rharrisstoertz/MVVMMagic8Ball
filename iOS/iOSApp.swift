a85b45182ceb36b9051f292ccb0de760d5e6b59c
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-09-20 14:38:20 -0400

    re-organized folders

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Harris-Stoertz, Rowan on 2021-09-20.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
